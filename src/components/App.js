c1a147acf160876ac71ffe026be812a449b55353
Vikas Verma <vikasverma15@gmail.com>
2026-01-07 21:31:54 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,41 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchLorem } from "../redux/loremSlice";
+import { setLoading, setPosts } from "../redux/loremSlice";
 
 const App = () => {
   const dispatch = useDispatch();
-
-  const { loading, data } = useSelector((state) => state.lorem);
-  const post = data && data[0] ? data[0] : {};
+  const { loading, posts } = useSelector((state) => state.lorem);
 
   useEffect(() => {
-    dispatch(fetchLorem());
+    dispatch(setLoading());
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => dispatch(setPosts(data.slice(0, 5))));
   }, [dispatch]);
 
   return (
     <div>
-      {/* FIRST HEADING */}
-      <h1>Lorem Redux</h1>
-
-      {/* SECOND HEADING */}
-      <h4>A short Naration of Lorem Ipsum</h4>
+      {/*  INTRO TEXT */}
+      <h1>A short Naration of Lorem Ipsum</h1>
+      <h4>
+        Below Contains A title and Body gotten froma random API, Please take your
+        time to Review
+      </h4>
 
       <ul>
+        {/* ALWAYS RENDER ONE STABLE <li> */}
         <li>
           <p className="title">
             {loading
-              ? "Title :Loading titles"
-              : `Title :${post.title}`}
+              ? "Title :Loading tiltes"
+              : `Title :${posts[0]?.title}`}
           </p>
 
           <p className="body">
             {loading
               ? "Body :Loading Body"
-              : `Body :${post.body}`}
+              : `Body :${posts[0]?.body}`}
           </p>
         </li>
       </ul>
